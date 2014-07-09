cd25a0dc6628e4a7498f78247e15e14430d52aac
idursun <ibrahim.dursun@gmail.com>
2014-07-09 17:48:25 +0300

    add login page

--- frontend/assets/javascripts/app/app.js
+++ frontend/assets/javascripts/app/app.js
@@ -9,7 +9,41 @@ define(['angular'], function (angular) {
   app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider
             .otherwise('home')
+
     $stateProvider.state('home', { url: '/', template: '<p>merhaba millet</p>' })
+    $stateProvider.state('login', { url: '/login', templateUrl: 'templates/login.html' })
+  })
+
+  app.config(function($httpProvider) {
+      $httpProvider.interceptors.push(function($q, $location) {
+          return {'responseError' : function(rejection) {
+              if(rejection.status == 401) {
+                  $location.url('/login')
+              }
+              return $q.reject(rejection);
+          }}
+      })
+  })
+
+  app.config(function(RestangularProvider) {
+    RestangularProvider.setBaseUrl('/rest/')
+  })
+
+  app.directive('dialog', function($state) {
+      return {
+          restrict: 'A',
+          link: function(scope, elem, attrs, ctrl) {
+              scope.$on('$stateChangeSuccess', function(event, toState) {
+                  if ($state.current.name == toState.name) {
+                      elem.modal('show')
+                  }
+              })
+
+              elem.on('hidden.bs.modal', function(e) {
+                  $state.go('^')
+              })
+          }
+      }
   })
 
   return app;
