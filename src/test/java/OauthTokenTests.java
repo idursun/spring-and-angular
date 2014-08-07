d69900e9333d14c4fb51bd139048a86d7231c357
idursun <ibrahim.dursun@gmail.com>
2014-08-07 16:56:45 +0300

    add WebApplicationContextSettings to oauth tests

--- src/test/java/OauthTokenTests.java
+++ src/test/java/OauthTokenTests.java
@@ -1,25 +1,53 @@
+import com.idursun.mvc1.Application;
+import com.idursun.mvc1.models.Project;
 import com.idursun.mvc1.services.ProjectRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleDataJpaApplication.class)
-public class OauthTokenTests {
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class OAuthTokenTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
 
     @Autowired
     ProjectRepository projectRepository;
 
-    @Test
-    public void testCount() {
+    @Autowired
+    FilterChainProxy springSecurityFilter;
 
-        assertThat(projectRepository.count(), is(0L));
+    @Before
+    public void setUp() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).addFilters(this.springSecurityFilter).build();
 
+        Project project = new Project();
+        project.setId(1L);
+        project.setName("Project1");
+        project.setCreatedBy(null);
+        projectRepository.save(project);
     }
 
+    @Test
+    public void testNotAuthorized() throws Exception {
+        mvc.perform(get("/rest/projects/")).andExpect(status().isUnauthorized());
+    }
 }
