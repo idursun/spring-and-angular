41d9d0a4e41d72d08887d30a613c7905e02f9c8d
idursun <ibrahim.dursun@gmail.com>
2014-08-08 15:45:42 +0300

    rename file to match class name

--- src/test/java/OauthTokenTests.java
+++ src/test/java/OauthTokenTests.java
@@ -4,15 +4,12 @@ import com.idursun.mvc1.services.ProjectRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -25,15 +22,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OAuthTokenTests {
 
     @Autowired
-    private WebApplicationContext context;
-
-    private MockMvc mvc;
-
-    @Autowired
     ProjectRepository projectRepository;
-
     @Autowired
     FilterChainProxy springSecurityFilter;
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
 
     @Before
     public void setUp() {
