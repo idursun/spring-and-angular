0e24e74f14ea4a6b69a0e53059744933aedf709d
idursun <ibrahim.dursun@gmail.com>
2014-09-24 16:21:21 +0300

    add pagination to name search

--- src/main/java/com/idursun/mvc1/services/ProjectRepository.java
+++ src/main/java/com/idursun/mvc1/services/ProjectRepository.java
@@ -1,14 +1,13 @@
 package com.idursun.mvc1.services;
 
-
 import com.idursun.mvc1.models.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource(collectionResourceRel = "projects", path = "projects")
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
-    List<Project> findByNameContains(@Param("name") String name);
+    Page<Project> findByNameContains(@Param("name") String name, Pageable pageable);
 }
